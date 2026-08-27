b9f8f92eed094efc8dc7342449af013954fba212
ᴹᴿツᎮᴇʀꜰᴇᴄᴛ࿐ <168732974+harishjat77@users.noreply.github.com>
2026-08-27 22:50:17 +0530

    Update API_ID and API_HASH in config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ import os
 class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8714119859:AAHNEqUMoyhpY3bquVQVDBR9XXS4yQ1Reuo")
-    API_ID = int(os.environ.get("API_ID", "31595997"))
-    API_HASH = os.environ.get("API_HASH", "4e94f5b04d2c2fb0b275f50f01347257")
+    API_ID = int(os.environ.get("API_ID", "35201189"))
+    API_HASH = os.environ.get("API_HASH", "d7b7c2cb7f08e16c9b10638dad8b9795")
     AUTH_USERS = os.environ.get("AUTH_USERS", "6410653364")
